6f1d151555f2604991cd5612ca04ce967196ea30
Jordan Todd Levine <Jordan.T.Levine@gmail.com>
2018-02-09 11:37:43 -0500

    Created dataframe

--- script.R
+++ script.R
@@ -3,3 +3,21 @@ d <- 4+5
 #Modifications
 complicate <- 1 +2
 #This is cool
+
+a <- 1 + 2 + 3
+library(tidyverse)
+b <- 1+2 +3
+d <- c(1,1,1)
+e <- c (2,2,2)
+f <- c(3,3,3)
+
+
+
+
+
+
+
+
+
+df <- data.frame(d, e, f)
+df
